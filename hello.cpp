7fe4b176bec437f2ede78ab0f3538952519ebe52
Olegzandr Denman <alex@oresoftware.com>
2017-01-03 15:48:46 -0800

    link

--- hello.cpp
+++ hello.cpp
@@ -20,19 +20,19 @@ using namespace std;
 
 void Method(const v8::FunctionCallbackInfo<v8::Value>& args) {
 
-  if (args.Length() < 2) {
-    Nan::ThrowTypeError("Wrong number of arguments");
-    return;
-  }
+    if (args.Length() < 2) {
+        Nan::ThrowTypeError("Wrong number of arguments");
+        return;
+    }
 
-fstream f;
+    fstream f;
 
-  if (!args[0]->IsString()) {
-    Nan::ThrowTypeError("First argument to 'replace-line' must be the absolute file path.");
-    return;
-  }
+    if (!args[0]->IsString()) {
+        Nan::ThrowTypeError("First argument to 'replace-line' must be the absolute file path.");
+        return;
+    }
 
-  //infile.open("/home/oleg/dogs.txt", fstream::in | fstream::out);
+    //infile.open("/home/oleg/dogs.txt", fstream::in | fstream::out);
 //  f.open("/home/oleg/dogs.txt", fstream::in);
     v8::String::Utf8Value filepath(args[0]->ToString());
     f.open(*filepath, fstream::in);
@@ -41,10 +41,16 @@ fstream f;
 
 //std::regex reg1;
 
-  if (!args[1]->IsString()) {
-    Nan::ThrowTypeError("Second argument to 'replace-line' must be a string regular expression.");
-    return;
-  }
+    if (!args[1]->IsString()) {
+        Nan::ThrowTypeError("Second argument to 'replace-line' must be a string regular expression.");
+        return;
+    }
+
+    v8::String::Utf8Value arg2(args[2]->ToString());
+    if (strcmp(*arg2, "true") == 0 && strcmp(*arg2, "false") == 0) {
+        Nan::ThrowTypeError("Third argument to 'replace-line' must be a string representing whether to find or replace.");
+        return;
+    }
 
 
     v8::String::Utf8Value target(args[1]);
@@ -55,7 +61,11 @@ fstream f;
 //    int priority = args[3]->ToNumber()->NumberValue();
 //    int priority_search_cap = args[4]->ToNumber()->NumberValue();
 
-    int count = 1;
+    bool isReplace = strcmp(*arg2, "true") == 0 ? true : false;
+    int count = args[3]->IsUndefined() ? 1 : args[2]->ToNumber()->NumberValue();
+    int priority = args[4]->IsUndefined() ? 0 : args[3]->ToNumber()->NumberValue();
+    int priority_search_cap = args[5]->IsUndefined() ? 20 : args[4]->ToNumber()->NumberValue();
+
 //    int priority = args[3]->ToNumber()->NumberValue();
 //    int priority_search_cap = args[4]->ToNumber()->NumberValue();
 //
@@ -70,63 +80,61 @@ fstream f;
 
 
 
+    if(f.fail()) {
+        cerr << " => 'replace-line' usage error =>  infile fail => path to file was likely invalid. " << endl;
+        exit(1);
+    }
+
+    if(!f.is_open()) {
+        cerr << " file could not be opened " << endl;
+        exit(1);
+    }
+
+    vector<string> v;
+    vector<vector<int>> ints;
+
+    string line;
+    int index = 0;
+
+    while (getline(f, line) && index < count) {
+
+        string str(line);
+        std::smatch match;
+
+        long old_offset = f.tellp()-str.length() -1;
+
+        if (std::regex_search(str, match, reg1) && match.size() > 0) {
+
+            index++;
+            long new_offset = f.tellp();
+
+            v.push_back(str);
+
+            if(old_offset > 1) {
+                old_offset--;
+            }
+
+            long len = new_offset -old_offset-1;
+            vector<int> c = {old_offset,len};
+            ints.push_back(c);
+        }
+
+    }
+
+    f.close();
+
+    fstream z;
+    z.open(*filepath);
 
 
-if(f.fail()){
-  cerr << " => 'replace-line' usage error =>  infile fail => path to file was likely invalid. " << endl;
-  exit(1);
-}
-
-if(!f.is_open()){
-  cerr << " file could not be opened " << endl;
-  exit(1);
-}
-
-vector<string> v;
-vector<vector<int>> ints;
-
-string line;
-int index = 0;
-
-while (getline(f, line) && index < count){
-
-     string str(line);
-     std::smatch match;
-
-     long old_offset = f.tellp()-str.length() -1;
-
-     if (std::regex_search(str, match, reg1) && match.size() > 0) {
-
-        index++;
-       long new_offset = f.tellp();
-
-       v.push_back(str);
-
-        if(old_offset > 1){
-           old_offset--;
-         }
-
-        long len = new_offset -old_offset-1;
-       vector<int> c = {old_offset,len};
-       ints.push_back(c);
-   }
-
-}
-
-f.close();
-
-fstream z;
-z.open(*filepath);
-
-
-  v8::Isolate* isolate = args.GetIsolate();
+    v8::Isolate* isolate = args.GetIsolate();
 //    v8::Local<String> retval = String::NewFromUtf8(isolate, "world");
     v8::Local<v8::Array> a = v8::Array::New(isolate);
 
 
 
 
-for(std::vector<int>::size_type i = 0; i < v.size(); i++) {
+    for(std::vector<int>::size_type i = 0; i < v.size(); i++) {
 
 
 //    v8::Local<String> vvv = v[i];
@@ -139,71 +147,79 @@ for(std::vector<int>::size_type i = 0; i < v.size(); i++) {
 
 //    v8::Local<v8::String> prop = v8::String(isolate, "dog");
 
-    a->Set(i, v8::String::NewFromUtf8(isolate, v[i].c_str()));
+        a->Set(i, v8::String::NewFromUtf8(isolate, v[i].c_str()));
 
-    int pos = ints[i][0];
-    int len = ints[i][1];
+        if(isReplace) {
 
-     z.seekp (pos);
-     string s(len, ' ');
-//     s = "\n" + s;
-      z << s;
+            int pos = ints[i][0];
+            int len = ints[i][1];
 
-}
+            z.seekp (pos);
+            string s(len, ' ');
+            //     s = "\n" + s;
+            z << s;
+
+        }
 
 
-  z.close();
+    }
 
 
-  fstream x;
-  x.open(*filepath, fstream::in);
+    z.close();
 
-  int ind = 0;
+    if(isReplace) {
 
-  int off = -1;
-  int leng = -1;
+        fstream x;
+        x.open(*filepath, fstream::in);
 
-  while (getline(x, line) && ind < 1){
+        int ind = 0;
 
-     string str(line);
+        int off = -1;
+        int leng = -1;
 
-     char h = str[0];
-     char p = str[str.length()-1];
+        while (getline(x, line) && ind < 1) {
+
+            string str(line);
+
+            char h = str[0];
+            char p = str[str.length()-1];
 
 //       cout << " p " << p << endl;
 
-       bool hIsChar = std::isspace(static_cast<unsigned char>(h));
-       bool pIsChar = std::isspace(static_cast<unsigned char>(p));
+            bool hIsChar = std::isspace(static_cast<unsigned char>(h));
+            bool pIsChar = std::isspace(static_cast<unsigned char>(p));
 
-    if (hIsChar && !pIsChar) {
+            if (hIsChar && !pIsChar) {
 //        cout << " c is a space " << endl;
-        leng = str.length();
+                leng = str.length();
 //         cout << " leng " << leng << endl;
 
-        off = x.tellp()- leng;
+                off = x.tellp()- leng;
 
 //        cout << " off " << off << endl;
-    }
+            }
 
-  }
+        }
 
-  x.close();
+        x.close();
 
 
-  if(off > -1){
+        if(off > -1) {
 
 //  cout << " off is on " << endl;
 
-fstream m;
-m.open(*filepath);
+            fstream m;
+            m.open(*filepath);
 
-   m.seekp (off);
-     string xx(leng + 1, ' ');
+            m.seekp (off);
+            string xx(leng + 1, ' ');
 //     s = "\n" + s;
-      m << xx;
+            m << xx;
 
-    m.close();
-  }
+            m.close();
+        }
+
+    } // end if isReplace
 
 
 
@@ -212,7 +228,7 @@ m.open(*filepath);
 }
 
 void init(v8::Local<v8::Object> exports) {
-NODE_SET_METHOD(exports, "hello", Method);
+    NODE_SET_METHOD(exports, "hello", Method);
 }
 
 NODE_MODULE(hello_addon, init)
--- test/a.js
+++ test/a.js
@@ -1,3 +1,7 @@
 const addon = require('../build/Release/hello.node');
 
-const val = addon.hello('/home/oleg/dogs.txt','\\S+');
+const val = addon.hello('/home/oleg/dogs.txt','\\S+', "false");
+
+const util = require('util');
+
+console.log('val => \n', util.inspect(val));
